44e1c98e897a11140c203beaadc533571781341b
Vedran Dragicevic <vdragicevic@iolap.com>
2023-05-02 15:47:21 +0200

    updated basic logger

--- logger.py
+++ logger.py
@@ -1,10 +1,10 @@
-""" Default logger setup """
+""" Default logger setup with custom format. """
 import logging
 import sys
 
 logging.basicConfig(
-        format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+        format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s (%(filename)s:%(lineno)d)',
         stream=sys.stdout,
         level=logging.INFO)
-logger = logging.getLogger("query_script")
+logger = logging.getLogger("lambda_script")
 logger.setLevel(logging.INFO)
